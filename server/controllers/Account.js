b3615d7ffba201218bbf718784d02e64e3528c1d
Allie Maus <aem3780@rit.edu>
2022-03-22 16:41:15 -0400

    start on domomaker A

---
+++ server/controllers/Account.js
@@ -0,0 +1,29 @@
+const models = require('../models');
+const Account = models.Account;
+
+const loginPage = (req, res) => {
+    return res.render('login');
+};
+
+const signupPage = (req, res) => {
+    return res.render('signup');
+};
+
+const logout = (req, res) => {
+    return res.render('/');
+};
+const login = (req, res) => {
+
+};
+
+const signup = (req, res) => {
+
+};
+
+module.exports = {
+    loginPage,
+    signupPage,
+    login,
+    logout,
+    signup,
+};
---
+++ server/controllers/Domo.js
@@ -0,0 +1,7 @@
+const makerpage = (req, res) => {
+    res.render('app');
+}
+
+module.exports = {
+    makerpage,
+};
