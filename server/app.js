7bcdd9abc417b0488e4ec834fff0446b9867d402
Allie Maus <aem3780@rit.edu>
2022-03-26 14:17:34 -0400

    fixed error, finsihed domoA

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
+const dbURI = 'mongodb+srv://alliemaus:June1301Pepper13@cluster0.damx6.mongodb.net' || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI, (err) => {
   if (err) {
     console.log('Could not connect to database');
